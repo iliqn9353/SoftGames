83a0622ab98f408d70da5d65817e881ed7d2c1df
iliqn9353 <55882205+iliqn9353@users.noreply.github.com>
2020-12-14 20:51:21 +0200

    deployed and fixed email !

--- Web/SoftGamesShop.Web/Controllers/ContactsController.cs
+++ Web/SoftGamesShop.Web/Controllers/ContactsController.cs
@@ -1,13 +1,13 @@
 ﻿namespace SoftGamesShop.Web.Controllers
 {
+    using System.Net;
+    using System.Net.Mail;
     using System.Threading.Tasks;
 
-    using MailKit.Net.Smtp;
     using Microsoft.AspNetCore.Mvc;
-    using MimeKit;
-    using MimeKit.Text;
     using SoftGamesShop.Data.Common.Repositories;
     using SoftGamesShop.Data.Models;
+    using SoftGamesShop.Web.ViewModels.Contacts;
 
     public class ContactsController : Microsoft.AspNetCore.Mvc.Controller
     {
@@ -28,42 +28,41 @@
 
         [Microsoft.AspNetCore.Mvc.HttpPost]
         [System.Obsolete]
-        public async Task<IActionResult> SendMail(ContactsModel contactsModel)
+        public async Task<IActionResult> SendMail(ContactFormViewModel contactsModel)
         {
-            if (this.ModelState.IsValid)
+            var fromAddress = new MailAddress("softgamesshop@gmail.com", contactsModel.Name);
+            var toAddress = new MailAddress("iliqn9353@gmail.com", "To Name");
+            var subject = contactsModel.Title;
+            var body = contactsModel.Content;
+            const string fromPassword = "101020Ik";
+
+            var smtp = new System.Net.Mail.SmtpClient
             {
-                var message = new MimeMessage();
-
-                message.To.Add(new MailboxAddress("iliqn9353@gmail.com"));
-
-                message.From.Add(new MailboxAddress("softgamesshop@gmail.com"));
-
-                message.Subject = contactsModel.Subject;
-
-                message.Body = new TextPart(TextFormat.Html)
-                {
-                    Text = contactsModel.Message + " Message was sent by: " + contactsModel.Name + " E-mail: " + contactsModel.Email,
-                };
-
-                using (var emailClient = new SmtpClient())
-                {
-                    emailClient.Connect("smtp.gmail.com", 587, false);
-                    emailClient.Authenticate("softgamesshop@gmail.com", "101020Ik"); // Delete before deploy !
-                    emailClient.Send(message);
-                    emailClient.Disconnect(false);
-                }
-
-                var email = new ContactsModel
-                {
-                    Name = contactsModel.Name,
-                    Email = contactsModel.Email,
-                    Message = contactsModel.Message,
-                    Subject = contactsModel.Subject,
-                };
-                await this.contactRepository.AddAsync(email);
-                await this.contactRepository.SaveChangesAsync();
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body + " Message was sent by: " + contactsModel.Name + " E-mail: " + contactsModel.Email,
+            })
+            {
+                smtp.Send(message);
             }
 
+            var email = new ContactsModel
+            {
+                Name = contactsModel.Name,
+                Email = contactsModel.Email,
+                Message = contactsModel.Content,
+                Subject = contactsModel.Title,
+            };
+            await this.contactRepository.AddAsync(email);
+            await this.contactRepository.SaveChangesAsync();
             this.TempData[RedirectedFromContactForm] = true;
             return this.RedirectToAction("ThankYou");
         }
